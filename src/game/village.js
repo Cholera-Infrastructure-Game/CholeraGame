efdc3f9277af37b5e7fb15c2fba7939eb72229f8
Derek Wu <derekwu@Dereks-MacBook-Pro.local>
2014-10-28 01:23:34 -0400

    added village js

---
+++ src/game/village.js
@@ -0,0 +1,13 @@
+function Village(population, parent_village) {
+	this.parent_village = parent_village;
+	this.total_population = population;
+	this.people_dead = 0;
+	this.people_alive = total_population - people_dead;
+
+	// TODO
+	function nextDay() {
+
+	}
+	
+
+}
