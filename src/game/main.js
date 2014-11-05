5b7e2de25926ce7a557bd5606cab71cdc2000045
Harry Sanabria <sanabria@mit.edu>
2014-11-04 19:38:27 -0500

    added click listeners for villages.  actual text to display and positions are wip

--- src/game/main.js
+++ src/game/main.js
@@ -7,9 +7,28 @@
  */
 
 count = 0;
+villageTextBox = null;
+currently_selected_village = 0;
+
+var populateVillageInfoBox = function () {
+    if (villageTextBox != null) {
+        villageTextBox.destroy();
+    }
+    var text = "Village " + currently_selected_village + "\nPercent Infected: " + all_villages_percent_infected[currently_selected_village] + "%";
+    var style = { font: "12px Arial", fill: "#ff0044", align: "left" };
+    villageTextBox = game.add.text(0, game.world._height-25, text, style);
+};
+
+var generateListenerForVillage = function(index) {
+    return function() {
+        currently_selected_village = index;
+    };
+}
 
 var state = {
     init: function() {
+        // TODO: decide on actual money amounts
+        money = 1000
         // TODO: put in actual village factors
 
         all_villages_percent_infected = [0,0,0,0,0,0];
@@ -22,37 +41,46 @@ var state = {
             Village(200, [1,1,1,1,1,1], 4),
             Village(100, [1,1,1,1,1,1], 5)
         ];
-        // Create simple text display for current Phaser version
-        var text = "Phaser Version "+Phaser.DEV_VERSION + " works! Hallelujah!";
-        var style = { font: "24px Arial", fill: "#fff", align: "center" };
-        var t = game.add.text(this.world.centerX, this.world.centerY, text, style);
-        t.anchor.setTo(0.5, 0.5);
-
+        populateVillageInfoBox();
     },
     preload: function() {
         // STate preload logic goes here
+        game.load.image('map', 'assets/images/map.png');
+        game.load.image('village', 'assets/images/TempCityIcon.png');
     },
-    create: function(){
-      // State create logic goes here
+    create: function() {
+        // State create logic goes here
+        game.add.sprite(0, 0, "map");
+        var villageImages = [];
+        for (i = 0; i < villages.length; i++) {
+            var villageImage = game.add.sprite(100*(i+1), game.world.centerY, 'village');
+            villageImage.anchor.set(0.5);
+            villageImage.inputEnabled = true;
+            console.log(i);
+            var iCopy = i+0;
+            villageImage.events.onInputDown.add(generateListenerForVillage(i), this);
+            villageImages.push(villageImage);
+        }
     },
     update: function() {
+        populateVillageInfoBox();
+        money += 1;
         // Called 60 times per second
         count += 1;
         if (count == 60) {
             count = 0;
             for (i = 0; i < villages.length; i++) {
-                villages[i].incrementDay();
+                // villages[i].incrementDay();
             }
             all_villages_percent_infected = temp_villages_percent_infected.slice();
         }
     }
 
-    // Don't you need to update temp to actual? Confused here
 };
 
 var game = new Phaser.Game(
-    800,
-    480,
+    1024,
+    1056,
     Phaser.AUTO,
     'game',
     state
